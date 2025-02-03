b6f2f5395c3a642a3ac342ebf6b4c9da5a4b71fe
taitai <taitai_pc@outlook.jp>
2025-02-03 20:02:46 +0900

    aiの難易度普通の作成。

--- ai.js
+++ ai.js
@@ -6,11 +6,80 @@ export function ai(field, difficulty_level = false) {
         availableIndices.push(i);
       }
     }
-    if (availableIndices.length === 0) return null; // 全ての値が0の場合、nullを返す
-    const randomIndex = Math.floor(Math.random() * availableIndices.length +1);
-    console.log(availableIndices)
+    const randomIndex = availableIndices[Math.floor(Math.random() * availableIndices.length)]; // ランダムなインデックスを取得
     return randomIndex;
     
   }
-  return random();
+  function reach_judge(a, player_number) { // リーチ判定
+    let row1 = a.slice(0, 3);
+    let row2 = a.slice(3, 6);
+    let row3 = a.slice(6, 9);
+    let col1 = [a[0], a[3], a[6]];
+    let col2 = [a[1], a[4], a[7]];
+    let col3 = [a[2], a[5], a[8]];
+    let dia1 = [a[0], a[4], a[8]];
+    let dia2 = [a[2], a[4], a[6]];
+    let rows = [row1, row2, row3, col1, col2, col3, dia1, dia2];
+
+    let reach_availability = rows.filter(row => row.includes(0) && 
+        (row.filter(x => x === player_number).length === 2));
+
+    if(reach_availability.length === 0) return [null];
+
+    let winning_moves = [];
+    for (let i = 0; i < rows.length; i++) { // リーチがあるか
+        if (rows[i].includes(0) && rows[i].filter(x => x === player_number).length === 2) { // リーチがある場合のパターンを取得
+            const emptyIndex = rows[i].indexOf(0); // 空いているインデックスを取得
+            let boardIndex; // boardIndexはどこを指せばいいのかを示す変数
+            switch(i) { // rowsの仲間ごとの場合分け
+                case 0: // 横判定 1
+                    boardIndex = emptyIndex;
+                    break;
+                case 1: // 横判定 2
+                    boardIndex = emptyIndex + 3;
+                    break;
+                case 2: // 横判定 3
+                    boardIndex = emptyIndex + 6;
+                    break;
+                case 3: // 縦判定 1
+                    boardIndex = emptyIndex * 3;
+                    break;
+                case 4: // 縦判定 2
+                    boardIndex = (emptyIndex * 3) + 1;
+                    break;
+                case 5: // 縦判定 3
+                    boardIndex = (emptyIndex * 3) + 2;
+                    break;
+                case 6: // 斜め判定 1
+                    if (emptyIndex === 0) boardIndex = 0;
+                    else if (emptyIndex === 1) boardIndex = 4;
+                    else if (emptyIndex === 2) boardIndex = 8;
+                    break;
+                case 7: // 斜め判定 2
+                    if (emptyIndex === 0) boardIndex = 2;
+                    else if (emptyIndex === 1) boardIndex = 4;
+                    else if (emptyIndex === 2) boardIndex = 6;
+                    break;
+            }
+            winning_moves.push(boardIndex);
+        }
+    }
+    console.log(rows);
+    return winning_moves;
+
+  }
+  function normal_selecting(){
+    if (reach_judge(field, 2)[0] !== null){ //自分にリーチがある場合
+      return reach_judge(field, 2)[0];
+    }else if (reach_judge(field, 1)[0] !== null){ //相手にリーチがある場合
+      return reach_judge(field, 1)[0];
+    }else{
+      return random();
+    }
+  }
+  if (!difficulty_level){ //難易度が簡単な場合
+    return normal_selecting();
+  }else{ //難易度が難しい場合
+    return normal_selecting();
+  }
 }
--- tictactoe.js
+++ tictactoe.js
@@ -1,8 +1,11 @@
-import {ai} from './ai.js';
-console.log(ai());
+import {ai} from './ai.js'; // aiをインポート
+
 let field = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 let player = true;
+let start_player = true;
 let battle_result = [0,0,0]
+let mode = "PlayervsPlayer"; // モードがPlayer vs PlayerかPlayer vs AIか
+let difficulty_level = false; // 難易度が簡単かどうか(簡単な場合はfalse)
 function judge(a) { // 判定
   function judge_same(b) { // 同じ値か判定
     if ((b[0] === b[1] && b[1] === b[2]) && (b[0] !== 0)) {
@@ -29,12 +32,42 @@ function resetGame(allclear = false) { // ゲームをリセット
     player = true;
   }
   field = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  if(mode !== "PlayervsAI"){
+    player = !start_player;
+    start_player = !start_player;
+  }else{
+    player = true;
+  }
   // ボタンのテキストをクリア
   document.querySelectorAll('button').forEach(button => button.innerText = '');
+  
 }
 
 function show(winner) { // 勝った時の表示
   alert(winner + 'の勝ち');
+}
+
+function battle_finish(scoreboard) { // ゲーム終了時の処理
+  if (judge(field)) { // 勝った時の処理
+    const winner = player ? "〇" : "×";
+    if (player) {
+      battle_result[0] += 1;
+    } else {
+      battle_result[1] += 1;
+    }
+    setTimeout(() => {
+      show(winner);
+      resetGame(); // ゲームをリセット
+      scoreboard.innerText = `〇が${battle_result[0]}勝、×が${battle_result[1]}勝、引き分けは${battle_result[2]}回です。`;
+    }, 100); // 少し遅延
+  }else if(!field.includes(0)){ // 引き分けの処理
+    setTimeout(() => {
+      alert('引き分け');
+      battle_result[2] += 1;
+      resetGame(); // ゲームをリセット
+      scoreboard.innerText = `〇が${battle_result[0]}勝、×が${battle_result[1]}勝、引き分けは${battle_result[2]}回です。`;
+    }, 100); // 少し遅延
+  }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -44,54 +77,47 @@ document.addEventListener('DOMContentLoaded', () => {
   const difficultyRadioButtons = document.querySelectorAll('input[name="difficulty_level"]');
   difficultyRadioButtons.forEach(button => button.disabled = true);
   modeRadioButtons.forEach(button => { 
-    button.addEventListener('change', (event) => { 
+    button.addEventListener('change', (event) => {  // モードが変更された時の処理を登録
       if (event.target.value === "PlayervsAI") { // AIと対戦する場合
         difficultyRadioButtons.forEach(button => button.disabled = false); 
-
+        mode = "PlayervsAI";
       }else {
         difficultyRadioButtons.forEach(button => button.disabled = true);
+        mode = "PlayervsPlayer";
       }
       resetGame(true);
     }
   );
   });
-  difficultyRadioButtons.forEach(button => { 
+  difficultyRadioButtons.forEach(button => {  // 難易度が変更された時の処理を登録
     button.addEventListener('change', (event) => { 
       resetGame(true);
+      difficulty_level = !difficulty_level;
     }
   );
   });
   buttons.forEach(button => { // ボタンがクリックされた時の処理を登録
     button.addEventListener('click', (event) => { // ボタンがクリックされた時の処理
       const buttonId = event.target.id;
-      if(field[buttonId[buttonId.length - 1] - 1] === 0){
+      if(field[buttonId[buttonId.length - 1] - 1] === 0){ // 既に埋まっている場合は何もしない
         if (player) { // 先攻後攻判定
           field[buttonId[buttonId.length - 1] - 1] = 1;
         } else {
           field[buttonId[buttonId.length - 1] - 1] = 2;
         }
         event.target.innerText = player ? '〇' : '×';
-        if (judge(field)) { // 勝った時の処理
-          const winner = player ? "〇" : "×";
-          if (player) {
-            battle_result[0] += 1;
-          } else {
-            battle_result[1] += 1;
-          }
-          setTimeout(() => {
-            show(winner);
-            resetGame(); // ゲームをリセット
-            scoreboard.innerText = `〇が${battle_result[0]}勝、×が${battle_result[1]}勝、引き分けは${battle_result[2]}回です。`;
-          }, 100); // 少し遅延
-        }else if(!field.includes(0)){ // 引き分けの処理
-          setTimeout(() => {
-            alert('引き分け');
-            battle_result[2] += 1;
-            resetGame(); // ゲームをリセット
-            scoreboard.innerText = `〇が${battle_result[0]}勝、×が${battle_result[1]}勝、引き分けは${battle_result[2]}回です。`;
-          }, 100); // 少し遅延
+        battle_finish(scoreboard); // ゲームが終了したか判定＆終了時の処理
+        if(mode === "PlayervsAI" && !judge(field) && field.includes(0)){ // AIと対戦する場合
+          player = !player; // 先攻後攻切り替え
+          let aiMove = ai(field, difficulty_level); // AIの手を取得
+          console.log(aiMove)
+          field[aiMove] = 2;
+          const ai_button = document.getElementById(`Button${aiMove + 1}`); // AIの手を表示
+          ai_button.innerText = '×';
+          console.log(field);
+          battle_finish(scoreboard); // ゲームが終了したか判定＆終了時の処理
         }
-        player = !player; // 後攻に交代
+        player = !player; // 先攻後攻切り替え
       }
     });
   });
